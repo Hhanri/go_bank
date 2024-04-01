cf7b8cefa50b2d68db416b1de1060cf00873f8ed
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-01 16:14:30 +0200

    api structure scaffolding

---
+++ api.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, r.Response.StatusCode, ApiError{err.Error()})
+		}
+	}
+}
+
+type ApiServer struct {
+	listenAddress string
+}
+
+func NewApiServer(listenAddress string) *ApiServer {
+	return &ApiServer{
+		listenAddress: listenAddress,
+	}
+}
+
+func (s *ApiServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+
+	log.Println("JSON API server running on port")
+
+	http.ListenAndServe(s.listenAddress, router)
+}
+
+func (s *ApiServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+
+	switch r.Method {
+	case "GET":
+		return s.handleGetAccount(w, r)
+	case "POST":
+		return s.handleCreateAccount(w, r)
+	case "DELETE":
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return fmt.Errorf("method not allowed: %s", r.Method)
+}
+
+func (s *ApiServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
